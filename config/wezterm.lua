d96cb0be21cede927386f1c4e507ad1db1438e6f
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2026-01-11 23:46:26 +0100

    Add smart-splits config with nvim compat

--- config/wezterm.lua
+++ config/wezterm.lua
@@ -21,6 +21,49 @@ config.colors = {
 config.font_size = 19
 
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+
+config.window_decorations = "RESIZE"
+-- config.window_background_opacity = 0.8
+-- config.macos_window_background_blur = 10
+
+local function is_vim(pane)
+	-- this is set by the plugin, and unset on ExitPre in Neovim
+	return pane:get_user_vars().IS_NVIM == "true"
+end
+
+local direction_keys = {
+	Left = "h",
+	Down = "j",
+	Up = "k",
+	Right = "l",
+	-- reverse lookup
+	h = "Left",
+	j = "Down",
+	k = "Up",
+	l = "Right",
+}
+
+local function split_nav(resize_or_move, key)
+	return {
+		key = key,
+		mods = resize_or_move == "resize" and "META" or "CTRL",
+		action = wezterm.action_callback(function(win, pane)
+			if is_vim(pane) then
+				-- pass the keys through to vim/nvim
+				win:perform_action({
+					SendKey = { key = key, mods = resize_or_move == "resize" and "META" or "CTRL" },
+				}, pane)
+			else
+				if resize_or_move == "resize" then
+					win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
+				else
+					win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
+				end
+			end
+		end),
+	}
+end
+
 config.keys = {
 	-- splitting
 	{
@@ -38,10 +81,17 @@ config.keys = {
 		key = "m",
 		action = wezterm.action.TogglePaneZoomState,
 	},
+	-- move between split panes
+	split_nav("move", "h"),
+	split_nav("move", "j"),
+	split_nav("move", "k"),
+	split_nav("move", "l"),
+	-- resize panes
+	split_nav("resize", "h"),
+	split_nav("resize", "j"),
+	split_nav("resize", "k"),
+	split_nav("resize", "l"),
 }
-config.window_decorations = "RESIZE"
--- config.window_background_opacity = 0.8
--- config.macos_window_background_blur = 10
 
 -- and finally, return the configuration to wezterm
 return config
