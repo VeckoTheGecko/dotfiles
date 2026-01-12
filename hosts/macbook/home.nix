ff098086ce25ade6cc87a3e9ce20dd0fb5256039
Vecko <36369090+VeckoTheGecko@users.noreply.github.com>
2026-01-12 10:30:29 +0100

    Disable kanata on mac

--- hosts/macbook/home.nix
+++ hosts/macbook/home.nix
@@ -14,6 +14,6 @@
     ../../modules/home/cli.nix
     ../../modules/home/starship.nix
     ../../modules/home/wezterm.nix
-    ../../modules/home/kanata.nix
+    # ../../modules/home/kanata.nix
   ];
 }
